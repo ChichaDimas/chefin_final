c111ad6f45617b63e360459928822c705d1f10a1
Delibero69 <deliberin@gmail.com>
2023-06-16 19:47:36 +0300

    ti dodik

--- chefin/store/models.py
+++ chefin/store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=128, blank=True)
+    category = models.CharField(max_length=128, blank=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=0, null=True, blank=True)
+    price_for_view = models.DecimalField(max_digits=10, decimal_places=0, null=True, blank=True)
+    image = models.ImageField(upload_to='products_images', blank=True)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'price': str(self.price),
+            # преобразование поля price в строку
+        }
+
+    def __str__(self):
+        return self.name
--- chefin/store/views.py
+++ chefin/store/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
 
-# Create your views here.
+from chefin.settings import POSTER_POS_API_KEY
+from django.shortcuts import render
+from .helpers import *
+from .models import *
+
+
+
 
 def menu(request):
+    api_key = POSTER_POS_API_KEY
+    fill_database(api_key)
+
+    query = request.GET.get('query')
+    category = request.GET.get('category')
+
+    products = Product.objects.all()
+
+    if query:
+        products = products.filter(name__icontains=query)
+
+    if category:
+        products = products.filter(category__icontains=category)
 
     context = {
         'title': 'Store - магазин',
-
+        'products': products,
     }
     return render(request, 'store/base.html', context)
